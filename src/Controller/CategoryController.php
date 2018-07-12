280e129e37ed8d74c7c4f2d74da6aaec81925b50
ChacaS0 <bastide.seb.pro@gmail.com>
2018-07-12 07:04:14 +0400

    auto generated Tag controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,89 +2,100 @@
 
 namespace App\Controller;
 
+use App\Utils\HTTP;
 use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * @Route("/category")
  */
 class CategoryController extends Controller
 {
-    /**
-     * @Route("/", name="category_index", methods="GET")
-     */
-    public function index(CategoryRepository $categoryRepository): Response
-    {
-        return $this->render('category/index.html.twig', ['categories' => $categoryRepository->findAll()]);
-    }
+	 /**
+	  * @Route("/", name="category_index", methods="GET")
+	  */
+	 public function index(CategoryRepository $categoryRepository): Response
+	 {
+		  return $this->render('category/index.html.twig', ['categories' => $categoryRepository->findAll()]);
+	 }
 
-    /**
-     * @Route("/new", name="category_new", methods="POST")
-     */
-    public function new(Request $request): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
+	 /**
+	  * @Route("/new", name="category_new", methods="POST")
+	  */
+	 public function new(Request $request): JsonResponse
+	 {
+			$code = HTTP::NO_RESPONSE; // default code
+			$msg = '';
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
+			$category;
+			try {
+				$category = new Category();
+				$form = $this->createForm(CategoryType::class, $category);
+				$form->handleRequest($request);
+	 
+				if ($form->isSubmitted() && $form->isValid()) {
+					$em = $this->getDoctrine()->getManager();
+					$em->persist($category);
+					$em->flush();
+				}
+				$code = HTTP::CREATED;
+		  } catch (Exception $ex) {
+				$code = HTTP::INTERNAL_ERROR;
+				$msg = $ex->getMessage();
+		  }
 
-            return $this->redirectToRoute('category_index');
-        }
+		  return new JsonResponse([
+				'code'		=> $code,
+				'message'	=> $msg,
+				'data'		=> ['category' => $category],
+		  ]);
+	 }
 
-        return $this->render('category/new.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
+	 /**
+	  * @Route("/{id}", name="category_show", methods="GET")
+	  */
+	 public function show(Category $category): Response
+	 {
+		  return $this->render('category/show.html.twig', ['category' => $category]);
+	 }
 
-    /**
-     * @Route("/{id}", name="category_show", methods="GET")
-     */
-    public function show(Category $category): Response
-    {
-        return $this->render('category/show.html.twig', ['category' => $category]);
-    }
+	 /**
+	  * @Route("/{id}/edit", name="category_edit", methods="POST")
+	  */
+	 public function edit(Request $request, Category $category): Response
+	 {
+		  $form = $this->createForm(CategoryType::class, $category);
+		  $form->handleRequest($request);
 
-    /**
-     * @Route("/{id}/edit", name="category_edit", methods="POST")
-     */
-    public function edit(Request $request, Category $category): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
+		  if ($form->isSubmitted() && $form->isValid()) {
+				$this->getDoctrine()->getManager()->flush();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+				return $this->redirectToRoute('category_edit', ['id' => $category->getId()]);
+		  }
 
-            return $this->redirectToRoute('category_edit', ['id' => $category->getId()]);
-        }
+		  return $this->render('category/edit.html.twig', [
+				'category' => $category,
+				'form' => $form->createView(),
+		  ]);
+	 }
 
-        return $this->render('category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
+	 /**
+	  * @Route("/{id}", name="category_delete", methods="DELETE")
+	  */
+	 public function delete(Request $request, Category $category): Response
+	 {
+		  if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+				$em = $this->getDoctrine()->getManager();
+				$em->remove($category);
+				$em->flush();
+		  }
 
-    /**
-     * @Route("/{id}", name="category_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Category $category): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($category);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('category_index');
-    }
+		  return $this->redirectToRoute('category_index');
+	 }
 }
